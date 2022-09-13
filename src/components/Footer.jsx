2abb66f9ba48b6d2ca7b653051fa6e08d53d767d
Micheal Leverton <levertonmicheal@gmail.com>
2022-09-13 17:43:39 -0400

    changing company name

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -24,7 +24,7 @@ const Footer = () => {
         </div>
 
         <p className="m-0 text-center text-white">
-          Copyright &copy; Summerville Dog Daycare 2022 - All Rights Reserved.
+          Copyright &copy; Summersville Dog Daycare 2022 - All Rights Reserved.
         </p>
 
       </div>
